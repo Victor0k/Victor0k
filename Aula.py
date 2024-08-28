caf91d94af36b1004b99c8c819ec6aa668c9cb53
Victor0k <victorbuava1@gmail.com>
2024-08-27 21:58:48 -0300

    Add files via upload

---
+++ Aula.py
@@ -0,0 +1,26 @@
+# print('Olá mundo')
+
+# minha_variavel = True
+# print(type(minha_variavel))
+
+# mult = float(3.4*2.3)
+# print(mult)
+
+# div = int(100//2)
+# print(div)
+
+# sub = int(54 - 23)
+# print(sub)
+
+# div = int(100//2)
+# print(div)
+
+# mult = int(45*34*42*34)
+# print(mult)
+
+# num = 42
+# num2 = 33
+
+# resutlado = num // num2
+# print(resutlado)
+
